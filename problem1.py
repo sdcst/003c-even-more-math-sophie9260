d92f3be0e6ef356c803f97a59fddd86bef025938
sophie9260 <158618434+sophie9260@users.noreply.github.com>
2024-04-15 12:39:40 -0700

    complete

--- problem1.py
+++ problem1.py
@@ -10,9 +10,7 @@ The Canadian income tax system is a tiered system, which means you pay different
 your income for different parts of your income.
 You pay 15% tax on the first 49020 you earn.  If you earn more than this amount, you pay
 20.5% on amounts over 49020 that are less than 98040
-
 26% on amounts over 98040 but less than 151978
-
 29% on amounts over 151978 but less than 216511
 33% on amounts over 216511
 
@@ -23,11 +21,48 @@ Enter your income: 125000
 Your federal income tax is: 24411.7
 
 '''
+
 question = "what is the income?"
 i = input(question)
 i = float(i)
+a = i - 49020
 
-t = i*0.26
-t = round(t,2)
+if i <= 49020: 
+    a = 0.15*i 
+    print(a)
 
-print(f"your federal income tex is: {t}$.")
+elif 49020 < i < 98040:
+    a= 49020*0.15
+    b = i - 49020
+    c = b*0.205
+    print(c+a)
+
+elif 98040 < i < 151978:
+    a= 49020*0.15
+    b= 49020*0.205
+    c = i - 98040
+    d = c*0.26
+    abd = a+b+d
+    abd = round(abd,2)
+    print(abd)
+
+elif 151978 < i < 216511:
+    a= 49020*0.15
+    b= 49020*0.205
+    c = 53938*0.26
+    d = i - 151978
+    e = 0.29*d
+    abce = a+b+c+e
+    abce = round(abce,2)
+    print(abce)
+
+elif i >= 216511:
+    a= 49020*0.15
+    b= 49020*0.205
+    c = 53938*0.26
+    d = 64533*0.29
+    e = i - 216511
+    f = 0.33*e
+    abcdf = a+b+c+d+f
+    abcdf = round(abcdf,2)
+    print(abcdf)
